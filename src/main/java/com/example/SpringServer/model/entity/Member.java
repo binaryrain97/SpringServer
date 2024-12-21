95911449de1977becd99e649697fd58bf7611950
binaryrain97 <binaryrain97@gmail.com>
2024-12-21 15:54:39 +0900

    first commit

---
+++ src/main/java/com/example/SpringServer/model/entity/Member.java
@@ -0,0 +1,27 @@
+package com.example.SpringServer.model.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class Member {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(unique = true)
+    private String email;
+
+    @Column
+    private String password;
+
+    @Column
+    private String nickname;
+}
